627d932d84bbad1d4e1bfc2fa196d0830379c2b4
jael-VRR <jrodriguezruidias@gmail.com>
2021-08-28 15:37:58 -0500

    run server

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const morgan =require('morgan');
 const app =express();
 
 //Settings
@@ -7,4 +8,5 @@ app.set('port',process.env.PORT || 3000)
 app.listen(app.get('port'), () => { 
     console.log(`Server on port ${app.get('port')}`);
 });
-
+//Middlewares
+app.use(morgan('dev'));
