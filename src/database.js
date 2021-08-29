01177ca214fc92f3b6d99a648b4c4fe95e76fa16
jael-VRR <jrodriguezruidias@gmail.com>
2021-08-29 05:10:56 -0500

    added database

---
+++ src/database.js
@@ -0,0 +1,9 @@
+
+const mongoose = require('mongoose');
+const URI = 'mongodb://localhost/mern-task';
+
+mongoose.connect(URI)
+  .then(db => console.log('Db is connected'))
+  .catch(error => console.error(error));
+
+module.exports = mongoose;
