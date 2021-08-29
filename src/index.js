397ab15cb5341173cd06f9597a9d76c36bdcfe24
jael-VRR <jrodriguezruidias@gmail.com>
2021-08-29 05:12:05 -0500

    conexion db

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
-const express = require('express');
-const morgan =require('morgan');
-const { dirname } = require('path');
-const path = require('path');
-const app =express();
+const express = require("express");
+const morgan = require("morgan");
+const path = require("path");
 
+const app = express();
+
+const { mongoose } = require('./database');
 //Settings
-app.set('port',process.env.PORT || 3000)
+app.set("port", process.env.PORT || 3000);
 
-app.listen(app.get('port'), () => { 
-    console.log(`Server on port ${app.get('port')}`);
+app.listen(app.get("port"), () => {
+  console.log(`Server on port ${app.get("port")}`);
 });
 //Middlewares
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
 
 //Routes
-app.use('/api/tasks',require('./routes/taskRoutes'));
+app.use("/api/tasks", require("./routes/taskRoutes"));
 
 //Static files
-console.log(__dirname);
-//app.use(express.static())
+app.use(express.static(path.join(__dirname, "public")));
