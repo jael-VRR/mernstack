d96490dbb07aa488157b0498bfd45323ab42af6a
jael-VRR <jrodriguezruidias@gmail.com>
2021-09-01 02:51:47 -0500

    install babel

--- webpack.config.js
+++ webpack.config.js
@@ -4,4 +4,13 @@ module.exports = {
     path: __dirname + "/src/public",
     filename: "bundle.js",
   },
+  module:{
+    rules: [
+      {
+        use: "babel-loader",
+        test: /\.js$/,
+        exclude: /node_modules/
+      }
+    ]
+  }
 };
